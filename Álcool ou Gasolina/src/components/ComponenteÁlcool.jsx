a3e82638056a5b6bbe02cb8df854f052dd9f2ab6
unknown <marinallfigueiredo@gmail.com>
2026-08-16 16:32:28 -0300

    alcool

---
+++ Álcool ou Gasolina/src/components/ComponenteÁlcool.jsx
@@ -0,0 +1,12 @@
+function ComponenteÁlcool({precoÁlcool, setPrecoÁlcool}) {
+    <section>
+        <div>
+            <label htmlFor="">Preço do Álcool (R$)</label>
+            <input>type="number" value="precoÁlcool"
+            onChange={(e) => setPrecoÁlcool(e.target.value)} placeholder="Ex: 3.49"
+            </input>
+        </div>
+    </section>
+}
+
+export default ComponenteÁlcool
