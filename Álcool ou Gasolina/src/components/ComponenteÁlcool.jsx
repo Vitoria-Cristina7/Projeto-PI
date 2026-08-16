563c4eb6e73ad3a3baf3746d782140ff8f9c00e8
unknown <marinallfigueiredo@gmail.com>
2026-08-16 16:46:49 -0300

    modi2

--- Álcool ou Gasolina/src/components/ComponenteÁlcool.jsx
+++ Álcool ou Gasolina/src/components/ComponenteÁlcool.jsx
@@ -2,8 +2,8 @@ function ComponenteÁlcool({precoÁlcool, setPrecoÁlcool}) {
     <section>
         <div>
             <label htmlFor="">Preço do Álcool (R$)</label>
-            <input>type="number" value="precoÁlcool"
-            onChange={(e) => setPrecoÁlcool(e.target.value)} placeholder="Ex: 3.49"
+            <input>type="number" value={precoÁlcool}
+            onChange={(e) => setPrecoÁlcool(e.target.value)} placeholder="Ex.: 3.49"
             </input>
         </div>
     </section>
